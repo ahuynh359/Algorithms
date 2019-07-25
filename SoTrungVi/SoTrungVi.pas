8c50341e95938291821aacd50b28cdcda555a560
Huynh Ngoc Anh <ahuynh359@gmail.com>
2019-07-25 15:32:48 +0700

    Update

--- SoTrungVi/SoTrungVi.pas
+++ SoTrungVi/SoTrungVi.pas
@@ -3,118 +3,64 @@ uses crt;
 const
         INPUT = 'SoTrungVi\Input.txt';
         OUTPUT = 'SoTrungVi\Output.txt';
-type mang = array[1..100,1..100] of integer;
-     b = array[1..100] of integer;
+type mang = array [1..100] of integer;
 var
-        a:mang;
+        a,kq,m:mang;
         n:integer;
-        i,j,k,l:integer;
+        i,j,k,l,tam,g:integer;
         f:text;
-        m,kq:b;
 
-procedure ReadData();
-
-BEGIN
-        assign(f,INPUT);
-        reset(f);
-
-        readln(f,n);
-        for i:=1 to n do
-                for j:=1 to n do
-                read(f,a[i,j]);
-
-        close(f);
-
-
-END;
-
-procedure WriteData();
-BEGIN
-        assign(f,OUTPUT);
-        rewrite(f);
-
-        write(f,kq[(1+n) div 2]);
-        close(f);
-END;
-
-procedure Swap(var a,b:integer);
+procedure Sort(var a:mang);
 var
-        tmp:integer;
+        i,j,tam:integer;
 BEGIN
-        tmp:=a;
-        a:=b;
-        b:=tmp;
-
-END;
-
-procedure Sort(var a:b;i,j:integer);
-var
-        pivot:integer;
-        left,right:integer;
-BEGIN
-        pivot:=a[(i+j) div 2];
-        left:=i;
-        right:=j;
-
-
-        repeat
-                while (a[left] < pivot) do inc(left);
-                while (a[right] > pivot) do dec(right);
-
-                if(left <= right) then
+        for i:=1 to n-1 do
+                for j:=i+1 to n do
+                if (a[i] > a[j]) then
                 begin
-                        Swap(a[left],a[right]);
-                        inc(left); dec(right);
+                        tam:=a[i];
+                        a[i]:=a[j];
+                        a[j]:=tam;
 
                 end;
 
-        until left > right;
-
-        if (left < j) then Sort(a,left,j);
-        if (right > i) then Sort(a,i,right);
-
-
-
-
-
-
-
 END;
 
-procedure ChangeArray(cot:integer);
-var
-        hang:integer;
-BEGIN
-        for hang:=1 to n do m[hang]:=a[cot,hang];
-
-END;
 
 BEGIN
 
         clrscr;
 
-        ReadData();
+        assign(f,INPUT);
+        reset(f);
 
-        j:=1;
+                readln(f,n);
 
-        for i:=1 to n do
-        begin
-                ChangeArray(i);
-                Sort(m,1,n);
-                kq[j]:=m[(1+n) div 2];
-                inc(j);
-
-
-
-        end;
-
-        Sort(kq,1,n);
+              while not(EOF(f)) do
+                begin
+                        for j:=1 to n do read(f,a[j]);
+                        Sort(a);
+                        inc(g);
+                        m[g]:=a[(1+n) div 2];
+                        readln(f);
+                        writeln;
 
 
-         for k:=1 to n do write(kq[k]:3);
+                end;
 
 
-        WriteData();
+
+
+
+        close(f);
+
+        assign(f,OUTPUT);
+        rewrite(f);
+        Sort(m);
+        write(f,m[(1+n) div 2]);
+        close(f);
+
+
         readln
 
 END.
