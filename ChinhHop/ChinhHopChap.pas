30b8aee566a3c8e97f7a60f2ff7ce1761728de5e
ahuynh359 <ahuynh359@gmail.com>
2020-08-13 02:33:50 +0700

    Update
    Update

--- ChinhHop/ChinhHopChap.pas
+++ ChinhHop/ChinhHopChap.pas
@@ -1,48 +1,32 @@
-program ChinhHop;
+program CHinhHopChapK;
 uses crt;
 var
-        i,l,n,j,k,dem:integer;
-        a:array[1..10] of integer;
-
+        i,j,k,v,b,n:integer;
+        a:array[1..100] of integer;
 
 BEGIN
         clrscr;
 
-
         write('Nhap n:');readln(n);
         write('Nhap k:');readln(k);
 
-
-        for i:=1 to k do a[i]:=1;
-
+        for i:=1 to k do a[i]:=i;
 
         repeat
-                inc(dem);
                 for i:=1 to k do write(a[i]:3);
                 writeln;
 
-                 i:=k;
-                while (i>0) and (a[i] = n) do dec(i); //Tim phan tu dau tien khac n
-                if(i>0) then
+                i:=k;
+                while (i>0) and (a[i] = n-k+i) do dec(i);
+                if ( i>0) then
                 begin
                         inc(a[i]);
-                        for j:=i+1 to k do a[j]:=1;
+                        for j:=i+1 to k do a[j]:=a[j-1]+1;
 
                 end;
 
 
 
-
-
-
-
-
         until i = 0;
-
-        writeln;
-        write('Co tong cong:', dem ,' chinh hop chap ', k,' cua ',n);
-
-
         readln
-
 END.
